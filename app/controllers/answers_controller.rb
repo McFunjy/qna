1f0fa2416d224c7c5f1ad3220e89f074dc22b600
McFunjy <barminov_01@mail.ru>
2021-08-17 17:22:11 +0300

    Add actions new, create and show for Answer controller and routes

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,31 @@
 class AnswersController < ApplicationController
+  before_action :find_question, only: %i[new create]
+  
+  def show
+    @answer = Answer.find(params[:id])
+  end
+
+  def new
+    @answer = Answer.new
+  end
+
+  def create
+    @answer = @question.answers.build(answer_params)
+
+    if @answer.save
+      redirect_to @answer
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
 end
