36d7e3f9f132513d42aa7ec33a491d3b7a884fef
McFunjy <barminov_01@mail.ru>
2021-08-17 17:17:50 +0300

    Add tests for new, create and show actions of Answer controller

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,2 +1,62 @@
 RSpec.describe AnswersController, type: :controller do
+  let(:answer) { create(:answer) }
+  let(:question) { create(:question) }
+
+  describe 'GET #new' do
+    before { get :new, params: { question_id: question } }
+
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'assigns a new Answer to @answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    let(:create_post) { post :create, params: { question_id: question, answer: answer_params } }
+
+    context 'with valid attributes' do
+      let(:answer_params) { attributes_for(:answer) }
+
+      it 'saves a new answer for question in the database' do
+        expect { create_post }.to change(question.answers, :count).by(1)
+      end
+
+      it 'redirects to show view' do
+        create_post
+        expect(response).to redirect_to assigns(:answer)
+      end
+    end
+
+    context 'with invalid attributes' do
+      let(:answer_params) { attributes_for(:answer, :invalid) }
+
+      it 'does not save the answer' do
+        expect { create_post }.to_not change(Answer, :count)
+      end
+
+      it 're-renders new view' do
+        create_post
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe 'GET #show' do
+    before { get :show, params: { id: answer } }
+
+    it 'assigns the requested answer to @answer' do
+      expect(assigns(:answer)).to eq answer
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+  end
 end
--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :answer do
     body { 'MyText' }
     question { build(:question) }
+
+    trait :invalid do
+      body { nil }
+    end
   end
 end
